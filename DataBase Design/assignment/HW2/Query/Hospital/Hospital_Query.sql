8a26f71424b7a66ab954268ee3021f6544144724
AmirHossein <amiryousefi98@gmail.com>
2019-04-09 21:26:59 +0430

    HW2 and related files

---
+++ DataBase Design/assignment/HW2/Query/Hospital/Hospital_Query.sql
@@ -0,0 +1,25 @@
+
+
+/*First question query*/
+select  max(price) from medicine where  id in 
+(select medicine_id from prescription where 
+(visit_id = 
+(select id from visit where (doctor_id = '1' and patient_id = '2'))));
+/*second question query*/
+create view max_profession as select profession,count(profession)  as 'cnt' from doctor  where doctor.id in (select doctor_id from visit
+ where visit.id in 
+ (select id from visit where room_number in
+ (select room_number from room where type ='2')))
+ group by profession order by 'cnt' limit 3;
+ 
+ select profession,cnt from max_profession where cnt in (select max(cnt) from max_profession);
+ 
+ /*third question query*/
+ select room_number from room where room_number not in (select room_number from supervision where supervision.nurse_id = '1');
+ /*forth question query*/
+create view phen_fen_medicine as select company,count(company) as 'cnt' from medicine where medicine.id in 
+(select id from medicine where (medicine.name like '%fen' or medicine.name like '%phen')) 
+group by company order by 'cnt' limit 4;
+
+select company,cnt from phen_fen_medicine where cnt in (select max(cnt) from phen_fen_medicine);
+ 
---
+++ DataBase Design/assignment/HW2/Query/Reservation/Reservation_Query.sql
@@ -0,0 +1,27 @@
+
+/*First question Query*/
+create view max_reject as 
+select teacher_id,count(teacher_id) as 'rejected'  from reserve where reserve.state = 'Rejected'
+ group by teacher_id order by 'rejected';
+ 
+select teacher_id , rejected from max_reject where rejected in 
+(select rejected from max_reject where rejected >2);
+/*second question Query*/
+
+select avg(salary) as 'salary average' from teacher where teacher_id  in
+(select  distinct teacher_id from reserve where room_number in 
+(select room_number from room where price in 
+(select max(price) from room))); 
+/*third question Query */
+
+create view mrc as select room_number, count(room_number) as 'RN' from reserve where (teacher_id = 1 and state = 'Approved') group by room_number
+order by 'RN';
+select capacity as 'max_repeated_capacity' from room where room_number in (select room_number from mrc where RN in (select max(RN) from mrc));
+
+
+/*forth question Query */
+create view days as select datediff(finishing_date , starting_date) as 'days'  from reserve where (teacher_id = 1 and state = 'Finished');
+select sum(days)as 'sum_reserve_days' from days;
+
+
+
